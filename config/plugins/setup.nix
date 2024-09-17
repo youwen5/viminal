d1db93ef00954e2b5cb546e2fab421353c661546
Youwen Wu <youwenw@gmail.com>
2024-09-17 01:39:07 -0700

    feat: add lualine lspprogress autocmd

--- config/plugins/setup.nix
+++ config/plugins/setup.nix
@@ -55,5 +55,12 @@
         return ""
       end,
     })
+
+    vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = "lualine_augroup",
+      pattern = "LspProgressStatusUpdated",
+      callback = require("lualine").refresh,
+    })
   '';
 }
