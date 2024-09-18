4a098743d2c906dc3bbaf75073a5a9092ec2f9a7
Youwen Wu <youwenw@gmail.com>
2024-09-18 00:55:19 -0700

    fix: use treesitter folding correctly

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -2,10 +2,10 @@
   plugins = {
     treesitter = {
       enable = true;
+      folding = true;
       settings = {
         highlight.enable = true;
         indent.enable = true;
-        folding.enable = true;
       };
     };
     treesitter-context.enable = true;
