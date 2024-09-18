914cd10148fd229df90689487bb9a52d3cadc702
Youwen Wu <youwenw@gmail.com>
2024-09-18 01:26:14 -0700

    feat: add persistent undofile

--- config/init.nix
+++ config/init.nix
@@ -3,11 +3,17 @@
   colorschemes.rose-pine.enable = true;
 
   opts = {
+    # Global status/cmdline
     laststatus = 3;
+
     relativenumber = true;
     number = true;
+    # sane defaults for tab/space
     shiftwidth = 2;
     tabstop = 4;
+    # Crank up fold level so tree-sitter doesn't fold everything
     foldlevel = 15;
+    # Persistent undos across sessions
+    undofile = true;
   };
 }
